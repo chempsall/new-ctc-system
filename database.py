9e76f97cc51a955164f5afbcc1745fcf8616d894
Chris Hempsall <christopher.hempsall@gmail.com~>
2026-07-02 22:13:33 +0100

    Step 1 of Fable 5 suggestions

--- database.py
+++ database.py
@@ -20,6 +20,8 @@ def get_connection():
         raise RuntimeError("SQLITE_PATH is not set. Check RF_DATABASE_URL in .env")
     conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
+    conn.execute("PRAGMA journal_mode=WAL")
+    conn.execute("PRAGMA busy_timeout=5000")
     conn.execute("PRAGMA foreign_keys = ON")
     return conn
 
